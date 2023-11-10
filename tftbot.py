e5c2deffee9c3734990c1f0d76c153e46aa6574c
JimmyJiang <tul70511@temple.edu>
2023-11-10 16:42:57 -0500

    make it so that u just enter the token and it will work

--- tftbot.py
+++ tftbot.py
@@ -1,6 +1,35 @@
 import discord
 from discord.ext import commands
 
+
+
+
+# Prompt the user to enter the token
+import discord
+from discord.ext import commands
+
+# Prompt the user to enter the token
+TOKEN = input("Enter your Discord bot token: ")
+
+# Create an instance of Intents
+intents = discord.Intents.default()
+intents.message_content = True
+
+# Pass the intents parameter to the Bot constructor
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+@bot.command(name='hi', help='Bot says hi')
+async def say_hi(ctx):
+    await ctx.send('Hi')
+
+bot.run(TOKEN)
+
+
+
 # Create an instance of Intents
 intents = discord.Intents.default()
 intents.message_content = True  
@@ -16,4 +45,4 @@ async def on_ready():
 async def say_hi(ctx):
     await ctx.send('Hi')
 
-bot.run('ODM0OTY2NDU0NDczNzE5ODM4.GA7muP.sP66ggaRg2B1aUFBIdGG1LsOFirES3eAvSyqyU')
+bot.run('Token')
