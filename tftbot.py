a713a2ec27a700922fa4507a97397c00553e82a8
jimmy <jimmyjiang70@gmail.com>
2023-11-30 17:10:57 -0500

    fix my main

--- tftbot.py
+++ tftbot.py
@@ -6,6 +6,7 @@ from config import *
 from discord.ext import commands
 from summoner_info import get_summoner_info
 from riotwatcher import TftWatcher
+from items import display_component
 
 #summoner url for information
 summoner_url = "https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name"
@@ -55,32 +56,8 @@ async def summoner_command(ctx, name):
 
 @bot.command(name='component', help='Get information on a specific component')
 async def display_component(ctx, component_name: str):
-    component_images = {
-        'tears': 'tears.png',
-        'spatula': 'spatula.png',
-        'gloves': 'gloves.png',
-        'belt': 'belt.png',
-        'cloak': 'cloak.png',
-        'rod': 'rod.png',
-        'bow': 'bow.png',
-        'sword': 'sword.png',
-    }
-
-    try:
-        # Get the image path based on the component name
-        image_path = component_images.get(component_name.lower())
-        if image_path:
-            with open(image_path, 'rb') as image_file:
-                file = discord.File(image_file, f'{component_name}.png')
-                await ctx.send(file=file)
-        else:
-            await ctx.send(f"Sorry, no information available for {component_name}.")
-    except FileNotFoundError:
-        await ctx.send(f"Sorry, the image file for {component_name} was not found.")
-    except Exception as e:
-        await ctx.send(f"An error occurred: {e}")
-
-
+    await display_component(ctx, component_name)
+ 
 
 
 
