b0bcd0f61fc524c2689c9f361e9a581456d0e10b
jimmy <jimmyjiang70@gmail.com>
2023-11-30 17:08:43 -0500

    add my components

--- tftbot.py
+++ tftbot.py
@@ -53,6 +53,32 @@ async def summoner_command(ctx, name):
 
 
 
+@bot.command(name='component', help='Get information on a specific component')
+async def display_component(ctx, component_name: str):
+    component_images = {
+        'tears': 'tears.png',
+        'spatula': 'spatula.png',
+        'gloves': 'gloves.png',
+        'belt': 'belt.png',
+        'cloak': 'cloak.png',
+        'rod': 'rod.png',
+        'bow': 'bow.png',
+        'sword': 'sword.png',
+    }
+
+    try:
+        # Get the image path based on the component name
+        image_path = component_images.get(component_name.lower())
+        if image_path:
+            with open(image_path, 'rb') as image_file:
+                file = discord.File(image_file, f'{component_name}.png')
+                await ctx.send(file=file)
+        else:
+            await ctx.send(f"Sorry, no information available for {component_name}.")
+    except FileNotFoundError:
+        await ctx.send(f"Sorry, the image file for {component_name} was not found.")
+    except Exception as e:
+        await ctx.send(f"An error occurred: {e}")
 
 
 
