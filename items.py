441caf715da1da4434f56e448ac824bfa5784f7d
jimmy <jimmyjiang70@gmail.com>
2023-11-30 17:48:07 -0500

    fixing error

--- items.py
+++ items.py
@@ -13,21 +13,22 @@ component_images = {
 }
 
 async def display_component(ctx, component_name: str):
-    try:
+    
         if component_name.lower() == 'list':
             items_list = ', '.join(component_images.keys())
             await ctx.send(f"Available components: {items_list}")
-        else:
+        
+            
+        try:
             # Get the image path based on the component name
             image_path = f'imageholder/{component_name.lower()}.png'
-            if os.path.exists(image_path):
+            if image_path:
                 with open(image_path, 'rb') as image_file:
                     file = discord.File(image_file, f'{component_name}.png')
                     await ctx.send(file=file)
             else:
                 await ctx.send(f"Sorry, no information available for {component_name}.")
-    except FileNotFoundError:
-        await ctx.send(f"Sorry, the image file for {component_name} was not found.")
-    except Exception as e:
-        await ctx.send(f"An error occurred: {e}")
-
+        except FileNotFoundError:
+            await ctx.send(f"Sorry, the image file for {component_name} was not found.")
+        except Exception as e:
+            await ctx.send(f"An error occurred: {e}")
