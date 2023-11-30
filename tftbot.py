5c0f79dfb75196f92e7f80c1428c65fb6cc98319
jimmy <jimmyjiang70@gmail.com>
2023-11-30 18:35:45 -0500

    fix get item

--- tftbot.py
+++ tftbot.py
@@ -30,24 +30,11 @@ async def say_hi(ctx):
     await ctx.send('Hi')
     
     
-
+  
 @bot.command(name='items', help='Get information on the basic components')
-async def display_image(ctx):
-    
+async def item_command(ctx):
+    await display_component(ctx)
 
-    image_path =  'items.png'
-
-    try:
-   
-        with open(image_path, 'rb') as image_file:
-            file = discord.File(image_file, 'items.png')
-            await ctx.send(file=file)
-    except FileNotFoundError:
-        await ctx.send(" the image file was not found.")
-    except Exception as e:
-        await ctx.send(f"An error occurred: {e}")
-
- 
  
 @bot.command(name='summoner', help='Get summoner information')
 async def summoner_command(ctx, name):
