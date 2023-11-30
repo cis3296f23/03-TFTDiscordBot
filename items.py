bb625234607da7ccc569635f8dc03749e8e59353
jimmy <jimmyjiang70@gmail.com>
2023-11-30 17:44:39 -0500

    fix item py now testing

--- items.py
+++ items.py
@@ -1,32 +1,33 @@
 import discord
 from discord.ext import commands
 
-
-
 component_images = {
-            'tears': 'tears.png',
-            'spatula': 'spatula.png',
-            'gloves': 'gloves.png',
-            'belt': 'belt.png',
-            'cloak': 'cloak.png',
-            'rod': 'rod.png',
-            'bow': 'bow.png',
-            'sword': 'sword.png',
-        }
-        
-        
-async def display_component(ctx, component_name: str):
+    'tears': 'tears.png',
+    'spatula': 'spatula.png',
+    'gloves': 'gloves.png',
+    'belt': 'belt.png',
+    'cloak': 'cloak.png',
+    'rod': 'rod.png',
+    'bow': 'bow.png',
+    'sword': 'sword.png',
+}
 
-        try:
+async def display_component(ctx, component_name: str):
+    try:
+        if component_name.lower() == 'list':
+            items_list = ', '.join(component_images.keys())
+            await ctx.send(f"Available components: {items_list}")
+        else:
             # Get the image path based on the component name
             image_path = f'imageholder/{component_name.lower()}.png'
-            if image_path:
+            if os.path.exists(image_path):
                 with open(image_path, 'rb') as image_file:
                     file = discord.File(image_file, f'{component_name}.png')
                     await ctx.send(file=file)
             else:
                 await ctx.send(f"Sorry, no information available for {component_name}.")
-        except FileNotFoundError:
-            await ctx.send(f"Sorry, the image file for {component_name} was not found.")
-        except Exception as e:
-            await ctx.send(f"An error occurred: {e}")
+    except FileNotFoundError:
+        await ctx.send(f"Sorry, the image file for {component_name} was not found.")
+    except Exception as e:
+        await ctx.send(f"An error occurred: {e}")
+
