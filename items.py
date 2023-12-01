3398084fd154e9089d1090625f73b3cf8c61b937
jimmy <jimmyjiang70@gmail.com>
2023-11-30 20:54:37 -0500

    fix component list so that the except dont function

--- items.py
+++ items.py
@@ -13,22 +13,23 @@ component_images = {
 }
 
 async def display_component(ctx, component_name: str):
-    
-        if component_name.lower() == 'list':
+    try:
+        component_name_lower = component_name.lower()
+
+        if component_name_lower == 'list':
             items_list = ', '.join(component_images.keys())
             await ctx.send(f"Available components: {items_list}")
-        
-            
-        try:
+
+        else:
             # Get the image path based on the component name
-            image_path = f'imageholder/{component_name.lower()}.png'
+            image_path = f'imageholder/{component_name_lower}.png'
             if image_path:
                 with open(image_path, 'rb') as image_file:
-                    file = discord.File(image_file, f'{component_name}.png')
+                    file = discord.File(image_file, f'{component_name_lower}.png')
                     await ctx.send(file=file)
-            else:
-                await ctx.send(f"Sorry, no information available for {component_name}.")
-        except FileNotFoundError:
-            await ctx.send(f"Sorry, the image file for {component_name} was not found.")
-        except Exception as e:
-            await ctx.send(f"An error occurred: {e}")
+
+    except FileNotFoundError:
+        await ctx.send(f"Sorry, the image file for {component_name_lower} was not found.")
+    except Exception as e:
+        await ctx.send(f"An error occurred: {e}")
+
