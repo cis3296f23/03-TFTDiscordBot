84ca0ef97f9a584c2fefc99fe337f78836446fd6
jimmy <jimmyjiang70@gmail.com>
2023-12-01 16:15:19 -0500

    fix ranking not showing the ranks so I made def

--- leaderboard.py
+++ leaderboard.py
@@ -10,14 +10,21 @@ async def get_tft_leaderboard(ctx, leaderboard_url, API_KEY):
         leaderboard_info = leaderboard_resp.json()
 
         top_10_entries = leaderboard_info['entries'][:10]
+        
+        # fix the display as its shown as I instead of Challenger, Gramaster etc so I need an def get_rank
+        def get_rank(rank):
+            
+            ranks= {{'I': 'Challenger', 'II': 'Grandmaster', 'III': 'Master'}}
+            return ranks(rank, rank)
 
         leaderboard_text = "Top 10 TFT Players:\n"
         for index, entry in enumerate(top_10_entries, start=1):
+            
             summoner_name = entry['summonerName']
             league_points = entry['leaguePoints']
             wins = entry['wins']
             losses = entry['losses']
-            rank = entry['rank']
+            rank = get_rank(entry['rank'])
 
             player_info = f"{index}. {summoner_name} ({rank}) - LP: {league_points} | Wins: {wins} | Losses: {losses}\n"
             leaderboard_text += player_info
