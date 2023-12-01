c0263fa0fe78215b9a3d34bcf20e67ba8765d41f
jimmy <jimmyjiang70@gmail.com>
2023-12-01 16:08:46 -0500

    fix my leaderboard to make it work and have the top 10 tft rank players

--- leaderboard.py
+++ leaderboard.py
@@ -1,4 +1,3 @@
-
 import requests
 
 async def get_tft_leaderboard(ctx, leaderboard_url, API_KEY):
@@ -8,9 +7,22 @@ async def get_tft_leaderboard(ctx, leaderboard_url, API_KEY):
         leaderboard_resp = requests.get(leaderboard_api_url)
         leaderboard_resp.raise_for_status()
 
-        leaderboard_info = leaderboard_resp.json()  # Adjust this based on your API response
+        leaderboard_info = leaderboard_resp.json()
 
-        await ctx.send(f"TFT Leaderboard:\n{leaderboard_info}")
+        top_10_entries = leaderboard_info['entries'][:10]
+
+        leaderboard_text = "Top 10 TFT Players:\n"
+        for index, entry in enumerate(top_10_entries, start=1):
+            summoner_name = entry['summonerName']
+            league_points = entry['leaguePoints']
+            wins = entry['wins']
+            losses = entry['losses']
+            rank = entry['rank']
+
+            player_info = f"{index}. {summoner_name} ({rank}) - LP: {league_points} | Wins: {wins} | Losses: {losses}\n"
+            leaderboard_text += player_info
+
+        await ctx.send(leaderboard_text)
 
     except Exception as e:
         await ctx.send(f"An error occurred: {e}")
