399b93d12b00f8bb7dce1dd663da9081ffd5e564
jimmy <jimmyjiang70@gmail.com>
2023-12-02 17:42:31 -0500

    added winrate now testing

--- rank.py
+++ rank.py
@@ -7,10 +7,8 @@ async def rank_info(ctx, name, summoner_url, rank_url, RIOT_GAMES_API_KEY):
         resp = requests.get(api_url)
         resp.raise_for_status()  # Check for errors in the response
 
-        # Parse summoner information
+        # Get Summoner Information 
         summoner_info = resp.json()
-
-        # Extract summoner ID
         summoner_id = summoner_info['id']
 
         # Rank information using summoner ID
@@ -23,10 +21,17 @@ async def rank_info(ctx, name, summoner_url, rank_url, RIOT_GAMES_API_KEY):
 
         if rank_info:
             tft_rank = f"TFT Rank: {rank_info[0]['tier']} {rank_info[0]['rank']}"
+            
+            # WinRate 
+            if 'wins' in rank_info[0] and 'losses' in rank_info[0]:
+                wins = rank_info[0]['wins']
+                losses = rank_info[0]['losses']
+                winrate = (wins / (wins + losses)) * 100
+                tft_rank += f"\nWins: {wins}, Lost: {losses}, Winrate: {winrate:.2f}%"
         else:
             tft_rank = "TFT Rank: Unranked"
 
         await ctx.send(f"{tft_rank}")
 
-    except requests.exceptions.RequestException as err:
-        await ctx.send(f"Error fetching summoner information: {err}")
+    except requests.exceptions.HTTPError as err:
+        await ctx.send(f"Error fetching match information: {err}")
