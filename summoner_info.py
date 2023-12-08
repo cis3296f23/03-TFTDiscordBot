b7043dbc5339338a5962789d336318c7ba5b20c0
FrancisRScallatino <FrancisRScallatino@gmail.com>
2023-12-07 22:55:59 -0500

    add pydoc comments to all 3 classes

--- summoner_info.py
+++ summoner_info.py
@@ -6,6 +6,17 @@ from discord.ext import commands
 
 
 async def get_summoner_info(ctx, name, summoner_url, rank_url, icon_url, matches_url, RIOT_GAMES_API_KEY):
+    """Used to find a particular summoner
+
+    Args:
+        ctx (Any): User input directly after slash character
+        name (Any): Name of the summoner
+        summoner_url (Any): URL for this particular player's profile
+        rank_url (Any): URL for this particular player's rank
+        icon_url (Any): URL for this particular players Icon
+        matches_url (Any): URL for this particular player's match history
+        RIOT_GAMES_API_KEY (Any): The Riot Games API key
+    """    
     try:
         # Summoner information
         api_url = f"{summoner_url}/{name}?api_key={RIOT_GAMES_API_KEY}"
@@ -48,6 +59,14 @@ async def get_summoner_info(ctx, name, summoner_url, rank_url, icon_url, matches
 
 
 async def get_match_info(ctx, name, specific_match_url, RIOT_GAMES_API_KEY):
+    """Used to retrieve information on a specific match
+
+    Args:
+        ctx (Any): User input directly after slash character
+        name (Any): Name of the summoner
+        specific_match_url (Any): URL for this particular player's match
+        RIOT_GAMES_API_KEY (Any): The Riot Games API key
+    """    
     try:
         match_id = name
         get_match = f"{specific_match_url}{match_id}/?api_key={RIOT_GAMES_API_KEY}"
@@ -66,7 +85,16 @@ async def get_match_info(ctx, name, specific_match_url, RIOT_GAMES_API_KEY):
 
 
 async def get_tft_leaderboard(ctx, API_KEY, region):
-    
+    """Used to retrieve the leaderboard for a given region
+
+    Args:
+        ctx (Any): User input directly after slash character
+        API_KEY (Any): the Riot Games API key
+        region (Any): The region in which to search for the leaderboard
+
+    Returns:
+        Any: The string to change the region for the leaderboard
+    """    
     base_url = f"https://{region}.api.riotgames.com/tft/league/v1/challenger"
     leaderboard_api_url = f"{base_url}?api_key={API_KEY}&region={region}"
 
@@ -124,7 +152,15 @@ async def get_tft_leaderboard(ctx, API_KEY, region):
         
 
         def check(reaction, user):
-            
+            """Checks when an emoji on the leaderboard output message is clicked on
+
+            Args:
+                reaction (String): the emoji clicked on
+                user (Any): The sender of the emoji
+
+            Returns:
+                Any: The string to change the region for the leaderboard
+            """            
             valid_emojis = {"😁", "🚀", "😄", "💻", "📚", "🎉", "🌟"}
             return (
                 user == ctx.author
@@ -169,6 +205,11 @@ async def get_tft_leaderboard(ctx, API_KEY, region):
         
 
 async def get_emoji(ctx):
+    """Finds given emoji
+
+    Args:
+        ctx (Any): The clicked emoji
+    """    
     image_path = 'emoji.png'
 
     try:
@@ -181,6 +222,15 @@ async def get_emoji(ctx):
         await ctx.send(f"An error occurred: {e}")
 
 async def rank_info(ctx, name, summoner_url, rank_url, RIOT_GAMES_API_KEY):
+    """Retrieves rank information for a given summoner
+
+    Args:
+        ctx (Any): User input directly after slash character
+        name (Any): Name of the summoner
+        summoner_url (Any): URL for the particular summoner's profile
+        rank_url (Any): URL for the particular sumoner's official rank
+        RIOT_GAMES_API_KEY (Any): The Riot Games API key
+    """    
     try:
         # Summoner information
         api_url = f"{summoner_url}/{name}?api_key={RIOT_GAMES_API_KEY}"
--- tftbot.py
+++ tftbot.py
@@ -4,6 +4,19 @@ from config import *
 from discord.ext import commands
 from summoner_info import *
 from items import *
+    
+"""This class handles starting up the bot and all of the commands a user can use
+
+Variables:
+    summoner_url {string} -- URL used for finding a given summoner
+    rank_url {string} -- URL used for finding the rank of a given summoner
+    icon_url {string} -- URL used for finding the icon a given summoner
+    matches_url {string} -- URL used for finding the match history a given summoner
+    specific_match_url {string} -- URL used for finding details of a specific match
+    
+    intents {Intents} -- creates an instance of a Discord Intents object to enable the bot functionality
+    bot {Bot} -- the bot object which you run commands through
+"""
 
 #summoner url for information
 summoner_url = "https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name"
@@ -22,40 +35,83 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 
 @bot.event
 async def on_ready():
+    """Used for letting the developer know the bot is active
+    
+    Returns:
+        a message to the console saying it's ready
+    """    
     print(f'Logged in as {bot.user.name}')
 
 @bot.command(name='hi', help='Bot says hi')
 async def say_hi(ctx):
+    """Makes the bot say "hi" in the text chat
+
+    Args:
+        ctx (Any): User input directly after slash character
+    """    
     await ctx.send('Hi')
     
   
 @bot.command(name='items', help='Get information on the basic components')
 async def item_command(ctx):
+    """Returns pictures with detailed descriptions of all tier 1 items
+
+    Args:
+        ctx (Any): User input directly after slash character
+    """    
     await display_image(ctx)
 
     
 @bot.command(name='summoner', help='Get summoner information')
 async def summoner_command(ctx, name):
+    """Outputs the given summoner level, PUUID, TFT Rank and the game codes for the last 20 TFT games they played
+
+    Args:
+        ctx (Any): User input directly after slash character
+        name (Ayn): name of the summoner the user wants to find
+    """    
     await get_summoner_info(ctx, name, summoner_url, rank_url, icon_url, matches_url, RIOT_GAMES_API_KEY)
 
 @bot.command(name='match', help='Get match information')
 async def match_command(ctx, name):
+    """Outputs statistics of a given game
+
+    Args:
+        ctx (Any): User input directly after slash character
+        name (Any): Unique match id
+    """    
     await get_match_info(ctx, name, specific_match_url, RIOT_GAMES_API_KEY)
 
 
 
 @bot.command(name='component', help='Get information on a specific component')
 async def component_commands(ctx, component_name: str):
+    """Outputs a picture of every possible build path for a given item
+
+    Args:
+        ctx (Any): User input directly after slash character
+        component_name (str): Name of item
+    """    
     await display_component(ctx, component_name)
  
 
 @bot.command(name='droprates', help='Get the dropRate of every tier in every level')
 async def drop_command(ctx):
+    """Outputs chart of drop rates for champions at particular levels
+
+    Args:
+        ctx (Any): User input directly after slash character
+    """    
     await display_drop(ctx)
 
 
 @bot.command(name='leaderboard' , help= 'Get the top ranking players information')
 async def tft_leaderboard(ctx):
+    """Outputs the top 10 players in a particular region (defaults to NA)
+
+    Args:
+        ctx (Any): User input directly after slash character
+    """    
     await get_emoji(ctx)
     await get_tft_leaderboard(ctx, RIOT_GAMES_API_KEY , "na1")
 
@@ -63,6 +119,12 @@ async def tft_leaderboard(ctx):
 
 @bot.command(name='rank', help= 'Get the TFT rank of the player with Winrate')
 async def rank_command(ctx, name):
+    """Outputs an image of the icon for the player rank, plus their specific rank in text and their W/L ratio
+
+    Args:
+        ctx (Any): User input directly after slash character
+        name (Any): Name of the summoner
+    """    
     await rank_info(ctx, name,summoner_url, rank_url, RIOT_GAMES_API_KEY)
 
 
